0d7196e0d9c57f52aaf7afb9f57700ab24919dfb
nafihkh <muhammednafih.k.hkottankodan@gmail.com>
2025-12-20 21:59:31 +0530

    BubbleSorting

---
+++ Bubblesorting.js
@@ -0,0 +1,17 @@
+function BubbleSort(arr){
+    let n = arr.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n-i-1; j++) {
+            if(arr[j]>arr[j+1])
+            {
+                let temp = arr[j];
+                arr[j]=arr[j+1]
+                arr[j+1]=temp;
+            }
+            
+        }
+        
+    }
+    console.log(arr)
+}
+BubbleSort([24,13,65,42])
